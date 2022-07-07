d23ba58e3714f21576b14a3efdea2425cd07d08a
frankowDan <eyitayoakande@outlook.com>
2022-07-06 19:36:50 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
+#include "main"
+/* print putchar */
 
-//print putchar
-
-int main();
+int main(char);
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	 _putchar("_");
+	 _putchar("p");
+	 _putchar("u");                                                                                                          
+	 _putchar("t");                                                                                                          
+	 _putchar("c");                                                                                                          
+	 _putchar("h");                                                                                                          
+	 _putchar("a");                                                                                                          
+	 _putchar("r");                                                                                                          
+	 _putchar("/n");
 
-	return (0);
+       	return (0);	 
+
+
 }
